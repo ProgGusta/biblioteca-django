db6e102d7101cb05847b8fcb316c3d490e865dc0
ProgGusta <gustavoprogpc@gmail.com>
2024-08-17 21:32:25 -0300

    correções

--- core/views.py
+++ core/views.py
@@ -1,31 +1,24 @@
-from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Livro
 from .serializers import LivroSerializer
-from rest_framework.renderers import JSONRenderer
-from django.http import HttpResponse
+from rest_framework.decorators import api_view
 
 # Create your views here.
-class JSONResponse(HttpResponse):
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-
 @csrf_exempt
+@api_view(['GET', 'POST'])
 def livro_list_create(request):
     if request.method == 'GET':
         livros = Livro.objects.all()
         serializer = LivroSerializer(livros, many=True)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
     if request.method == 'POST':
         serializer = LivroSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data, status=status.HTTP_201_CREATED)
-        return JSONResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @csrf_exempt
 def livro_detail(request, pk):
@@ -33,13 +26,13 @@ def livro_detail(request, pk):
 
     if request.method == 'GET':
         serializer = LivroSerializer(livro)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
     if request.method == 'PUT':
         serializer = LivroSerializer(livro, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     if request.method == 'DELETE':
         livro.delete()
-        return JSONResponse(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)
